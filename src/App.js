e4d291f999bd0232e578bc85a9c3d54963727c98
Soniya Jaiswal <91732415+soniiya@users.noreply.github.com>
2022-02-15 20:06:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useRef} from "react";
+import "./App.css";
 
-function App() {
+function App(){
+
+  const [todoList, setTodolist] = useState([]);
+  const [currentTask,setCurrenttask]=useState("");
+  
+  const inputTask=useRef(null);
+
+  const addTask = () =>{
+   setTodolist([...todoList, {task: currentTask, completed: false}]);
+   inputTask.current.value ="";
+  }
+
+  const deletetask = (taskToDelete)=>{
+    setTodolist(
+      todoList.filter((task)=>{               // will filter that task which is not equal to value(todeleteatsk) 
+                                              // rest will be printed.
+      return task.task !== taskToDelete;
+    }))
+  }
+
+  const completetask = (taskTocomplete)=>{
+    setTodolist(
+      todoList.map( (task) =>{
+        return task.task === taskTocomplete 
+        ? { task: taskTocomplete , completed:true} 
+        : {task: task.task, completed: task.completed ? true : false}
+      })
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>TODO-LIST</h1>
+    <div>
+      <input 
+      ref={inputTask}
+       type="text" placeholder="Type here..."
+       onKeyDown={(e) =>{
+         if(e.keyCode == 13) addTask();
+       }} 
+        onChange = {(e) =>{
+        setCurrenttask(e.target.value)
+        }}
+      />
+      <button onClick={addTask}>Add task</button>
+    </div>
+    <hr />
+    <ul>
+      {todoList.map((value,key)=>{
+        return(
+          <div id="task">
+          <li key={key}> {value.task}</li>
+          <button id="complete_btn" onClick={()=> completetask(value.task)  }>Completed</button>
+          <button id="del_btn" onClick={() => deletetask(value.task) }>Delete</button>
+          {value.completed ? <h1>Completed task</h1> : <h1>Not completed</h1>}
+          </div>
+        )
+      })}
+    </ul>
+  
     </div>
   );
-}
+  }
 
 export default App;
